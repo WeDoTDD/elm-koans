b4373c9e86fa73f5ba6f2e77dee8255333b1a177
dschinkel <dschinkel@gmail.com>
2018-07-26 14:47:15 -0500

    Expects Koan Passes

--- tests/Koans01AboutExpects.elm
+++ tests/Koans01AboutExpects.elm
@@ -27,15 +27,17 @@ testSuite =
         [ -- `test` takes the test description and a function that returns an
           -- `Expectation` when evaluated with the unit tuple `()`
           test "Expect.true tests for a true value"
-            (\() -> Expect.true "Should be True" (x____replace me____x))
+            (\() -> Expect.true "Should be True" (1 < 2))
+
           -- `<|` calls the function on the left with the argument on the right
           --  and can be used to avoid some parenthesis
         , test "Expect.equal tests for equality" <|
-            \() -> Expect.equal True (x____replace me____x)
+            \() -> Expect.equal True (1 == 1)
+
           -- `|>` calls the function on the right with the argument on the left
           -- and can be used to "pipeline" values through a series of functions
         , test "Expect.notEqual tests for inequality" <|
             \() ->
-                x____replace me____x
+                1 == 1
                     |> Expect.notEqual False
         ]
